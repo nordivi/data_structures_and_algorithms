6012c55bfde3381bf70c4cdaab4badc493d06cbc
nordivi <victorw.nordi@icloud.com>
2023-12-06 14:49:34 -0300

    O(1) -- py

---
+++ bigO/compress_first_box.py
@@ -0,0 +1,9 @@
+
+
+
+def log_first_two_boxes(boxes):
+    print(boxes[0]) # O(1)
+    print(boxes[1]) # O(1)
+    # Total # O(1) --- still constant, but we say O(1) anyway
+
+log_first_two_boxes([12,4,5,6,6,4,23,34])
