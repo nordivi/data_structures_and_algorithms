fd397b676bfa53ff164960a39bf68f76c95b7f18
nordivi <victorw.nordi@icloud.com>
2023-12-06 14:44:43 -0300

    O(n)

--- bigO/nemo.js
+++ bigO/nemo.js
@@ -1,11 +1,14 @@
-const nemo = ['nemo']
+const nemo = ['dasdsa', 'dsadas', 'nemo']
+const everyone = ['dory', 'bruce', 'marlin', 'nemo', 'fill']
+const large = new Array(1000).fill('nemo')
+
 
 function findNemo(array) {
-    for (let i = 0; i < array.length; i++){
+    for (let i = 0; i < array.length; i++){ // O(n) -- linear (depends on the number of inputs) --- n elements -> n operations
         if (array[i] === 'nemo') {
             console.log('FOUND NEMO!')
         }
     }
 }
 
-findNemo(nemo)
+findNemo(large)
