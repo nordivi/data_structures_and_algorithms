7f5c6a1d2672f4e6a8a23ce2598c9476526dda98
nordivi <victorw.nordi@icloud.com>
2023-12-07 21:21:28 -0300

    space complexity

--- bigO/space_complexity.py
+++ bigO/space_complexity.py
@@ -4,13 +4,14 @@ def boooo(n):
     for i in n:
         print('booo!')
 
+
 boooo([1,2,3,4,5]) # space complexity of O(1) -- i is not being used. its still O(n) time complexity
 
 
 def hi_n_times(n):
     hi_array = {}
     for i in range(n):
-        hi_array[i] = 'hi'
+        hi_array[i] = 'hi' # its using the index
     print(hi_array)
     return hi_array
 
